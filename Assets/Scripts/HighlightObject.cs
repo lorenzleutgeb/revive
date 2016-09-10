5efe8ccbc658375b40dc5dc9ef8c755c30819216
David Jöch <david.joech@catalysts.cc>
2016-09-10 12:11:53 +0200

    added pop-out animation

--- Assets/Scripts/HighlightObject.cs
+++ Assets/Scripts/HighlightObject.cs
@@ -4,27 +4,49 @@ using HoloToolkit.Unity;
 
 public class HighlightObject : MonoBehaviour {
 
-    public Vector3 Direction;
+    public Vector3 endPosition;
+    private Vector3 startPosition;
+
+    public float speed = 1f;
+
+    private float timeStartedLerping;
+    private bool isLerping;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        startPosition = this.transform.localPosition;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-        
+	void Update ()
+    {
+        if (isLerping)
+        {
+            timeStartedLerping += Time.deltaTime * speed;
 
+            this.transform.localPosition = Vector3.Lerp(startPosition, endPosition, timeStartedLerping);
+
+            if(timeStartedLerping >= 1.0f)
+            {
+                isLerping = false;
+            }
+        }
 	}
+
+    void StartLerping()
+    {
+        isLerping = true;
+        timeStartedLerping = 0;
+    }
 
     void OnGazeEnter()
     {
-        this.transform.localPosition += Direction;
+        StartLerping();
     }
 
     void OnGazeLeave()
     {
-        this.transform.localPosition -= Direction;
+        this.transform.localPosition = Vector3.zero;
     }
 }
